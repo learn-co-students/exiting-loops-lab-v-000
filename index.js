17efb4d75f6cfdce404e6df60a6f548c6b4d7315
Priti Khanna <khpriti@gmail.com>
2017-05-16 22:54:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 function breakOut(array, changeValue, stopValue) {
 
   for(var i =0; i<array.length; i++) {
-    var changeValue;
     if (changeValue != stopValue) {
-      changeValue = array[i];
+        array[i] = changeValue;
     }
     break;
   }
-  return array;
+  return array[i];
 }
 
 function keepGoing(array, changeValue, skipValue) {
   for(var i =0; i<array.length; i++) {
-    var changeValue;
 
     if (changeValue != skipValue) {
-      changeValue = array[i];
+      array[i] = changeValue;
     }
     continue;
   }
-  return array;
+  return array[i];
 }
 
 function findBy(array, findFn) {
-
+for(var i =0; i<array.length; i++) {
+if (findFn(array[i])) {
+  return array[i];
+}
+return null;
+}
 }
