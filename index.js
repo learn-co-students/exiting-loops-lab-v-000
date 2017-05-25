6e56fe68e7b4abf178bb4b1cbfa2945ffccbb690
Seth <itz.saga@gmail.com>
2017-05-24 22:13:00 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+"use strict";
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === stopValue) {
+      break
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+function findBy(array, findFn) {
+  let found = false;
+  for (let i = 0; i < array.length; i++) {
+    if (findFn(array[i]) === true) {
+      found = true;
+      return array[i]
+    }
+  }
+  if (found === false) {
+    return null
+  }
+}
