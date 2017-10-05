ef810063e64d350b285d4e4289248db5c5f369e7
James Fernandez <jffernan@comcast.net>
2017-10-05 15:22:36 -0700

    Re-do & added pseudocode // comments

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
+/* 
+1. Declare function named breakOut(array, changeValue, stopValue)
+2. Iterates through array and changes every element to changeValue until the loop reaches stopValue. 
+3. Then we break out of the loop and return the array.
+*/
 function breakOut(array, changeValue, stopValue) {
-  for (let i = 0, l = array.length; i < l; i++) {
+  for (var i in array) {
     if (array[i] === stopValue) {
-      break
+      break;
     }
-
-    array[i] = changeValue
+    array[i] = changeValue;
   }
-
-  return array
+  return array;
 }
 
+/* 
+1. Declare a function named keepGoing(array, changeValue, skipValue)
+2. Iterates through array and changes every element to changeValue except those that match skipValue.
+3. Then return the array.
+*/
 function keepGoing(array, changeValue, skipValue) {
   for (let i = 0, l = array.length; i < l; i++) {
     if (array[i] === skipValue) {
-      continue
+      continue;
     }
-
-    array[i] = changeValue
+    array[i] = changeValue;
   }
-
-  return array
+  return array;
 }
 
+/* 
+1. Declare a function named findBy(array, findFn) which looks for a value in array based on the return value of findFn. 
+2. Return null if the value isn't found.
+*/
 function findBy(array, findFn) {
   for (let i = 0, l = array.length; i < l; i++) {
     if (findFn(array[i])) {
-      return array[i]
+      return array[i];
     }
   }
-
-  return null
+  return null;
 }
