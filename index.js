66b4ed915f599fcb35ada4ef97fb8a20dfb79c58
smithwebtek <brad@smithwebtek.com>
2018-03-04 19:22:23 -0500

    Done.

--- index.js
+++ index.js
@@ -37,6 +37,47 @@ function findBy(array, findFn) {
 
 
 
+/////////////////////
+//   find(collections, (n => n > 3 && typeof n === 'string'))
+// 	 find(collections, (n => n === '6' && typeof n === 'string'))
+////////////////////
+
+const collections = [1, [2, [4, [5, [6]], 3]]]
+
+function find(array, criteriaFn) {
+	let current = array
+	let next = []
+
+	while (current) {
+		if (criteriaFn(current)) {
+			console.log('if true', criteriaFn(current), 'it is of type: ', typeof current)
+			return current
+
+		}
+		console.log('if false', criteriaFn(current), 'it is of type: ', typeof current)
+		if (Array.isArray(current)) {
+
+			for (let i = 0; i < current.length; i++) {
+				next.push(current[i])
+				console.log(next)
+			}
+
+		}
+
+		current = next.shift()
+
+	}
+
+	return null
+
+}
+
+
+
+
+
+
+
 
 
 
