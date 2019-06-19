b51ffcd3318ec869cd656b87ed77cdf84c32e9e9
Michael Pieschel <mpieschel39@gmail.com>
2019-06-19 18:47:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ function keepGoing(array, changeValue, skipValue) {
     }
     return array;
 }
+
+function loops findBy(array, findFn) {
+    let result;
+    for (let i = 0; i < array.length; i++)  {
+        if (array[i] === findFn) {
+            result = array[i];
+        }
+    }
+    return result;
+}
